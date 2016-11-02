6c7d9b0242a654a1374d154b2c061b43c450bbae
amariet <785037@seq.org>
2016-11-02 09:53:31 -0700

    Update Magpie3.java

--- MagpieActivityStarterCode/activity3/Magpie3.java
+++ MagpieActivityStarterCode/activity3/Magpie3.java
@@ -68,14 +68,12 @@ public class Magpie3
 	 * @return the index of the first occurrence of goal in
 	 *         statement or -1 if it's not found
 	 */
-	private int findKeyword(String statement, String goal,
-			int startPos)
+	private int findKeyword(String statement, String goal, int startPos)
 	{
 		String phrase = statement.trim();
 		// The only change to incorporate the startPos is in
 		// the line below
-		int psn = phrase.toLowerCase().indexOf(
-				goal.toLowerCase(), startPos);
+		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
 
 		// Refinement--make sure the goal isn't part of a
 		// word
@@ -83,35 +81,29 @@ public class Magpie3
 		{
 			// Find the string of length 1 before and after
 			// the word
-			String before = " ", after = " ";
+			String before = " "
+			String after = " ";
 			if (psn > 0)
 			{
-				before = phrase.substring(psn - 1, psn)
-						.toLowerCase();
+				before = phrase.substring(psn - 1, psn).toLowerCase();
 			}
 			if (psn + goal.length() < phrase.length())
 			{
-				after = phrase.substring(
-						psn + goal.length(),
-						psn + goal.length() + 1)
-						.toLowerCase();
+				after = phrase.substring(psn + goal.length(),psn + goal.length() + 1).toLowerCase();
 			}
 
 			// If before and after aren't letters, we've
 			// found the word
-			if (((before.compareTo("a") < 0) || (before
-					.compareTo("z") > 0)) // before is not a
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a
 											// letter
-					&& ((after.compareTo("a") < 0) || (after
-							.compareTo("z") > 0)))
+					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
 
 			// The last position didn't work, so let's find
 			// the next, if there is one.
-			psn = phrase.indexOf(goal.toLowerCase(),
-					psn + 1);
+			psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
 
 		}
 
