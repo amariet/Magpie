bb653ed8bb87836237ba14727f776c90e7e0c860
amariet <785037@seq.org>
2016-11-03 08:50:46 -0700

    Update Magpie3.java

--- MagpieActivityStarterCode/activity3/Magpie3.java
+++ MagpieActivityStarterCode/activity3/Magpie3.java
@@ -10,177 +10,207 @@
  */
 public class Magpie3
 {
-	/**
-	 * Get a default greeting
-	 * 
-	 * @return a greeting
-	 */
-	public String getGreeting()
-	{
-		return "Hello, let's talk.";
-	}
+    /**
+     * Get a default greeting
+     * 
+     * @return a greeting
+     */
+    public String getGreeting()
+    {
+        return "Hello, let's talk.";
+    }
 
-	/**
-	 * Gives a response to a user statement
-	 * 
-	 * @param statement
-	 *            the user statement
-	 * @return a response based on the rules given
-	 */
-	public String getResponse(String statement)
-	{
-		String response = "";
-		else if(findKeyword(statement, "yes") >= 0)
-		{
-			response = "Mmmm that's nice ;)";
-		}
-		else if(findKeyword(statement, "maybe") >= 0)
-		{
-			response = "What's the hold up, why don't you just pick a side godammit?!?!?!";
-		}
-		else if(findKeyword(statement, "okay") >= 0 
-		{
-			response = "Haha, and then what? ;)";
-		}
-		else if(findKeyword(statement, "depression") >= 0) 
-			|| findKeyword(statement, "sad") >= 0
-			|| findKeyword(statement, "existential feeling of self doubt") >= 0);
-		{
-			response = "Well we are all going to die one day, when the bell tolls, it tolls for thee and thee alone.";					
-		}
-		else if(findKeyword(statement, "low self esteem") >= 0
-		{
-			response = "It's okay, you don't have low self esteem, you just probably just have low esteem for other people. If not, get help.";
-		}
-		else if(findKeyword(statement, "people" >= 0)
-		{
-			response = "Yeah, people suck. It seems like I'm on another planet when I talk to them.";
-		}
-		if (findKeyword(statement, "no") >= 0)
-		{
-			response = "Why so negative?";
-		}
-		else if (findKeyword(statement, "mother") >= 0
-				|| findKeyword(statement, "father") >= 0
-				|| findKeyword(statement, "sister") >= 0
-				|| findKeyword(statement, "brother") >= 0)
-		{
-			response = "Tell me more about your family.";
-		}
-		else
-		{
-			response = getRandomResponse();
-		}
-		return response;
-	}
-			
-	/**
-	 * Search for one word in phrase. The search is not case
-	 * sensitive. This method will check that the given goal
-	 * is not a substring of a longer string (so, for
-	 * example, "I know" does not contain "no").
-	 * 
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @param startPos
-	 *            the character of the string to begin the
-	 *            search at
-	 * @return the index of the first occurrence of goal in
-	 *         statement or -1 if it's not found
-	 */
-	private int findKeyword(String statement, String goal, int startPos)
-	{
-		String phrase = statement.trim();
-		// The only change to incorporate the startPos is in
-		// the line below
-		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+    /**
+     * Gives a response to a user statement
+     * 
+     * @param statement
+     *            the user statement
+     * @return a response based on the rules given
+     */
+    public String getResponse(String statement)
+    {
+        String response = "";
+        if (findKeyword(statement, "no") >= 0)
+        {
+            response = "Why so negative?";
+        }
+        else if(findKeyword(statement, "yes") >= 0)
+        {
+            response = "Mmmm that's nice ;)";
+        }
+        else if(findKeyword(statement, "maybe") >= 0)
+        {
+            response = "What's the hold up, why don't you just pick a side godammit?!?!?!";
+        }
+        else if(findKeyword(statement, "okay") >= 0)
+        {
+            response = "Haha, and then what? ;)";
+        }
+        else if(findKeyword(statement, "depression") >= 0
+        || findKeyword(statement, "sad") >= 0
+        || findKeyword(statement, "existential feeling of self doubt") >= 0)
+        {
+            response = "Well we are all going to die one day, when the bell tolls, it tolls for thee and thee alone.";					
+        }
+        else if(findKeyword(statement, "low self esteem") >= 0)
+        {
+            response = "It's okay, you don't have low self esteem, you just probably just have low esteem for other people. If not, get help.";
+        }
+        else if(findKeyword(statement, "people") >= 0)
+        {
+            response = "Yeah, people suck. It seems like I'm on another planet when I talk to them.";
+        }
 
-		// Refinement--make sure the goal isn't part of a
-		// word
-		while (psn >= 0)
-		{
-			// Find the string of length 1 before and after
-			// the word
-			String before = " "
-			String after = " ";
-			if (psn > 0)
-			{
-				before = phrase.substring(psn - 1, psn).toLowerCase();
-			}
-			if (psn + goal.length() < phrase.length())
-			{
-				after = phrase.substring(psn + goal.length(),psn + goal.length() + 1).toLowerCase();
-			}
+        else if (findKeyword(statement, "mother") >= 0
+        || findKeyword(statement, "father") >= 0
+        || findKeyword(statement, "sister") >= 0
+        || findKeyword(statement, "brother") >= 0
+        || findKeyword(statement, "mom") >= 0
+        || findKeyword(statement, "dad") >= 0
+        || findKeyword(statement, "biological sister") >= 0
+        || findKeyword(statement, "biological brother") >= 0
+        || findKeyword(statement, "biological sibling") >= 0
+        || findKeyword(statement, "sibling") >= 0
+        || findKeyword(statement, "roach") >= 0
+        || findKeyword(statement, "parasite") >= 0
+        || findKeyword(statement, "the parasite that made me a middle child") >= 0
+        || findKeyword(statement, "twin") >= 0)
+        {
+            response = "Tell me more about your family.";
+        }
+        else if(findKeyword(statement, "dog") >= 0 
+        || findKeyword(statement, "cat") >= 0)
+        {
+            response = "Tell me more about your pets.";
+        }
+        else if(findKeyword(statement, "Mr. Ettlin") >= 0)
+        {
+            response = "I guess he's aight?";
+        }
+        else if(statement.trim().length() == 0)
+        {
+            response = "Say something please.";
+        }
+        else
+        {
+            response = getRandomResponse();
+        }
+        return response;
+    }
 
-			// If before and after aren't letters, we've
-			// found the word
-			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before is not a
-											// letter
-					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
-			{
-				return psn;
-			}
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no").
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @param startPos
+     *            the character of the string to begin the
+     *            search at
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal,int startPos)
+    {
+        String phrase = statement.trim();
+        // The only change to incorporate the startPos is in
+        // the line below
+        int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
 
-			// The last position didn't work, so let's find
-			// the next, if there is one.
-			psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+        // Refinement--make sure the goal isn't part of a
+        // word
+        while (psn >= 0)
+        {
+            // Find the string of length 1 before and after
+            // the word
+            String before = " ", after = " ";
+            if (psn > 0)
+            {
+                before = phrase.substring(psn - 1, psn)
+                .toLowerCase();
+            }
+            if (psn + goal.length() < phrase.length())
+            {
+                after = phrase.substring(
+                    psn + goal.length(),
+                    psn + goal.length() + 1)
+                .toLowerCase();
+            }
 
-		}
+            // If before and after aren't letters, we've
+            // found the word
+            if (((before.compareTo("a") < 0) || (before
+                    .compareTo("z") > 0)) // before is not a
+                // letter
+            && ((after.compareTo("a") < 0) || (after
+                    .compareTo("z") > 0)))
+            {
+                return psn;
+            }
 
-		return -1;
-	}
+            // The last position didn't work, so let's find
+            // the next, if there is one.
+            psn = phrase.indexOf(goal.toLowerCase(),
+                psn + 1);
 
-	/**
-	 * Search for one word in phrase. The search is not case
-	 * sensitive. This method will check that the given goal
-	 * is not a substring of a longer string (so, for
-	 * example, "I know" does not contain "no"). The search
-	 * begins at the beginning of the string.
-	 * 
-	 * @param statement
-	 *            the string to search
-	 * @param goal
-	 *            the string to search for
-	 * @return the index of the first occurrence of goal in
-	 *         statement or -1 if it's not found
-	 */
-	private int findKeyword(String statement, String goal)
-	{
-		return findKeyword(statement, goal, 0);
-	}
+        }
 
-	/**
-	 * Pick a default response to use if nothing else fits.
-	 * 
-	 * @return a non-committal string
-	 */
-	private String getRandomResponse()
-	{
-		final int NUMBER_OF_RESPONSES = 4;
-		double r = Math.random();
-		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
-		String response = "";
+        return -1;
+    }
 
-		if (whichResponse == 0)
-		{
-			response = "Interesting, tell me more.";
-		}
-		else if (whichResponse == 1)
-		{
-			response = "Hmmm.";
-		}
-		else if (whichResponse == 2)
-		{
-			response = "Do you really think so?";
-		}
-		else if (whichResponse == 3)
-		{
-			response = "You don't say.";
-		}
+    /**
+     * Search for one word in phrase. The search is not case
+     * sensitive. This method will check that the given goal
+     * is not a substring of a longer string (so, for
+     * example, "I know" does not contain "no"). The search
+     * begins at the beginning of the string.
+     * 
+     * @param statement
+     *            the string to search
+     * @param goal
+     *            the string to search for
+     * @return the index of the first occurrence of goal in
+     *         statement or -1 if it's not found
+     */
+    private int findKeyword(String statement, String goal)
+    {
+        return findKeyword(statement, goal, 0);
+    }
 
-		return response;
-	}
+    /**
+     * Pick a default response to use if nothing else fits.
+     * 
+     * @return a non-committal string
+     */
+    private String getRandomResponse()
+    {
+        final int NUMBER_OF_RESPONSES = 4;
+        double r = Math.random();
+        int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
+        String response = "";
+
+        if (whichResponse == 0)
+        {
+            response = "Interesting, tell me more.";
+        }
+        else if (whichResponse == 1)
+        {
+            response = "Hmmm.";
+        }
+        else if (whichResponse == 2)
+        {
+            response = "Do you really think so?";
+        }
+        else if (whichResponse == 3)
+        {
+            response = "You don't say.";
+        }
+
+        return response;
+    }
 
 }
